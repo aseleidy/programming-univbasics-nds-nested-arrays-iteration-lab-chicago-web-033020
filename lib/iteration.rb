14c76842d049a34860874d5e91bec62f1865d0b5
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-23 19:05:25 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -32,12 +32,12 @@ def find_greater_pair(src)
     
     if first_num < src[row_index][1]
       new_src << src[row_index][1]
-    else first_num == src[row_index][1]
-      new_src 
+    elsif first_num == src[row_index][1]
+      new_src
+    else
+      new_src << first_num
     end 
   
-    new_src << first_num
-    
     row_index += 1
   end
   
